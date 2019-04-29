8fe81c1a471cd2120d9c822f9d220bca18925e22
Taylor <taymosier@gmail.com>
2019-04-29 17:55:36 -0400

    finished billboard modal + major styling xlarge screens

--- client/src/Components/Billboards/BillboardModal.jsx
+++ client/src/Components/Billboards/BillboardModal.jsx
@@ -11,20 +11,24 @@ export class BillboardModal extends Component {
       isOpen: false,
     }
     this.toggleModal = this.toggleModal.bind(this);
+    this.getImageDimensionRatio = this.getImageDimensionRatio.bind(this);
+    this.toggleActiveImage = this.toggleActiveImage.bind(this);
+    this.toggleFullScreen = this.toggleFullScreen.bind(this);
+    this.setFullScreenImageStyle = this.setFullScreenImageStyle.bind(this);
+    this.getImageOptionPlaceholderStyle = this.getImageOptionPlaceholderStyle.bind(this);
   }
 
   componentDidMount(){
     this.setState({
-      isOpen: this.props.modal,
-      class: "",
-      toggle: this.props.toggle,
-      images: this.props.images,
-      multipleImages: typeof this.props.images === "object",
       activeImage: 0,
-      imageFull: false
+      class: "",
+      contents: this.props.contents,
+      imageFull: false,
+      images: this.props.images,
+      isOpen: this.props.modal,
+      multipleImages: typeof this.props.images === "object",
+      toggle: this.props.toggle
     })
-    this.toggleActiveImage = this.toggleActiveImage.bind(this);
-    this.toggleFullScreen = this.toggleFullScreen.bind(this);
   }
 
   componentDidUpdate(){
@@ -57,36 +61,66 @@ export class BillboardModal extends Component {
     })
   }
 
-  render(){
+  getImageDimensionRatio(){
+    let imageHeight = document.querySelector('.billboard-image').naturalHeight;
+    let imageWidth = document.querySelector('.billboard-image').naturalWidth;
+    let ratio;
+    if(imageHeight >= imageWidth){
+      ratio = imageWidth/imageHeight;
+    } else if (imageHeight < imageWidth){
+      ratio = imageHeight/imageWidth;
+    }
+    return ratio;
+  }
+
+  setFullScreenImageStyle(){
+    let ratio = this.getImageDimensionRatio();
+    let left = `${(100 - (100*ratio))/2}vw`;
     let fullScreen = {
-      "left": "1.25vw",
+      "left": left,
       "margin": "0",
-      "max-width": "97.5vw",
-      "max-height": "97.5vh",
-      "min-width": "97.5vw",
-      "min-height": "97.5vh",
+      "maWidth": `${100*ratio}vw`,
+      "maxHeight": "97.5vh",
+      "minWidth": `${100*ratio}vw`,
+      "minHeight": "97.5vh",
       "padding": "0",
       "position": "absolute",
       "top": "1vh",
-      "z-index": "100000"
+      "zIndex": "100000"
     }
+    return fullScreen
+  }
+
+  getImageOptionPlaceholderStyle(){
+    return {
+      "text-align": "center",
+      "width": "50%",
+      "minWidth": "50%",
+      "maxWidth": "50%",
+      "margin": "5%",
+      "minHeight": "fit-content",
+      "maxHeight": "fit-content",
+      "height": "fit-content"
+    }
+  }
+
+  render(){
+
     return(
       <Modal className="billboard-modal" style={this.state.imageFull ? {"margin": "0", "background": "transparent", "border": "none"} : null} isOpen={this.state.isOpen} toggle={this.state.toggle}>
         <ModalBody style={this.state.imageFull ? {"margin": "0", "background": "transparent", "border": "none"} : null}>
 
           {this.state.multipleImages
-            ?  <BillboardImage
-                className={`billboard-modal-image ${this.state.class}`} //Image Element in top left of modal screen
+            ?  <BillboardImage //Image Element in top left of modal screen
                 image={this.state.images[this.state.activeImage].src}
                 alt={this.state.images}
-                style={this.state.imageFull ? fullScreen : null}
+                style={this.state.imageFull ? this.setFullScreenImageStyle() : null}
                 toggle={this.toggleFullScreen}
                />
             : <BillboardImage
-                className={`billboard-modal-image ${this.state.class}`}
                 image={this.state.images}
                 alt={this.state.images}
-                style={this.state.imageFull ? fullScreen : null}
+                style={this.state.imageFull ? this.setFullScreenImageStyle() : null}
                 toggle={this.toggleFullScreen}
                />
           }
@@ -106,20 +140,20 @@ export class BillboardModal extends Component {
               )
             }
             </div>
-            : <div className="billboard-modal-image-options" /> //empty div if only one image
+            : <div className="billboard-modal-image-options" style={this.getImageOptionPlaceholderStyle()}>Floorplan</div>
           }
 
           <div className="billboard-modal-text" style={this.state.imageFull ? {"display": "none"} : null}>
             <p className="billboard-modal-text-header">Room Info</p>
             <div className="billboard-modal-main-text">
-              {this.props.contents.flavor_text.en !== undefined && this.state.imageFull
+              {this.props.contents.flavor_text.en !== undefined && !this.state.imageFull
                 ? splitText(this.props.contents.flavor_text.en, "billboard-modal-text-block") //returns an array of <p> elements
                 : null
               }
-              <Button className="toggle-image-fullscreen-button" onClick={this.toggleFullScreen}>View Full Screen</Button>
             </div>
+            <Button className="toggle-image-fullscreen-button" onClick={this.toggleFullScreen}>View Full Screen</Button>
+            <Button className="close-billboard-modal-button" onClick={this.props.toggle}>X</Button>
           </div>
-          <Button className="close-billboard-modal-button" onClick={this.props.toggle}>X</Button>
         </ModalBody>
       </Modal>
     )
